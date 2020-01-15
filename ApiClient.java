ece4a66f1eb9b00686d227f53032c6f488d35781
Khamad Ali <37427479+alicard@users.noreply.github.com>
2020-01-15 19:13:35 +0700

    Create ApiClient.java

---
+++ ApiClient.java
@@ -0,0 +1,20 @@
+package com.example.bengkelku;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+
+    public static final String BASE_URL = "http://192.168.100.7/bengkel/";
+    public static Retrofit retrofit;
+
+    public static Retrofit getApiClient(){
+        if (retrofit==null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
