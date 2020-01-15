4fe5eda27a84552903442d957fdeadbda0bb60f9
Khamad Ali <37427479+alicard@users.noreply.github.com>
2020-01-15 19:11:12 +0700

    Update maps.java

--- maps.java
+++ maps.java
@@ -3,9 +3,11 @@ package com.example.bengkelku;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.fragment.app.FragmentActivity;
@@ -14,21 +16,22 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
 import java.io.IOException;
 import java.util.List;
+
+import static com.example.bengkelku.MainActivity.bengkel;
 
 public class maps extends FragmentActivity implements OnMapReadyCallback {
 
     EditText etSearch;
     Button btnSearch;
-    static final LatLng ST3TELKOM_COORDINATE = new LatLng(-7.434952, 109.250123);
-    static final LatLng BATURADEN_COORDINATE = new LatLng(-7.298936, 109.216753);
 
     private GoogleMap mMap;
+    CameraPosition cameraPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,18 +74,25 @@ public class maps extends FragmentActivity implements OnMapReadyCallback {
         }
     }
 
+    // This Error Solved
+    public void getMarker() {
+        for (Bengkel data : bengkel) {
+            LatLng latlng = new LatLng(Double.parseDouble(data.getLat()), Double.parseDouble(data.getLng()));
+
+            mMap.addMarker(new MarkerOptions().position(latlng).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        LatLng defaultZoom = new LatLng(-7.759708, 110.408932);
 
         // Add a marker in Sydney, Australia, and move the camera.
-        Marker markerST3=mMap.addMarker(new MarkerOptions().position(ST3TELKOM_COORDINATE).icon(BitmapDescriptorFactory.fromResource(R.drawable.common_full_open_on_phone)).title("ST3 TELKOM PURWOKERTO").snippet("Bridging Technology for Humanity"));
-        Marker markerBaturaden=mMap.addMarker(new MarkerOptions().position(BATURADEN_COORDINATE).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)).title("BATURADEN").snippet("Tempat Wisata Purwokerto"));
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ST3TELKOM_COORDINATE, 30));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+        cameraPosition = new CameraPosition.Builder().target(defaultZoom).zoom(10).build();
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(defaultZoom, 17));
         mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        getMarker();
     }
-
 }
-
